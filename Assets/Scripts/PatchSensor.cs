9446e8c262650c7edae26dda5bec47247edb9d6f
jonahcancio <jonahcancio@gmail.com>
2018-03-01 20:08:18 +0800

    initial commit: hanggang tape dragging pa lang

---
+++ Assets/Scripts/PatchSensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchSensor : MonoBehaviour {
+	public bool hasBeenPatched;
+
+	void Start(){
+		hasBeenPatched = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		hasBeenPatched = true;
+		Debug.Log (gameObject.name + " has been patched");
+	}	
+
+	void OnTriggerExit2D(Collider2D other){
+		hasBeenPatched = false;
+		Debug.Log (gameObject.name + " has been unpatched");
+	}	
+
+}
---
+++ Assets/Scripts/TapeInstantiator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapeInstantiator : MonoBehaviour {
+
+	public GameObject tape;
+	private Transform tapeInstance;
+	public Transform door;
+
+
+	private string tapeMode;
+	private float scaleOverLengthRatio;
+	private Vector3 anchorPoint;
+	private Vector3 mousePoint;
+	private Rigidbody2D rb;
+
+	void Update () {
+		//create's a new tape set to drag mode where physics simulations have been disabled in its rigidbody component; no trigger colliders will be triggered
+		if (Input.GetMouseButtonDown (0)) {
+			door = GameObject.FindWithTag ("Door").transform;
+			anchorPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Debug.Log (door.GetComponent<SpriteRenderer> ().bounds);
+			Debug.Log (anchorPoint);
+			if ( door.GetComponent<SpriteRenderer> ().bounds.Contains (new Vector3(anchorPoint.x, anchorPoint.y)) ) {
+				tapeInstance = Instantiate (tape, new Vector3 (anchorPoint.x, anchorPoint.y), Quaternion.identity, door).transform.Find ("Tape Sprite");
+				scaleOverLengthRatio = tapeInstance.localScale.y / tapeInstance.GetComponent<BoxCollider2D> ().bounds.size.y;
+				rb = tapeInstance.GetComponent<Rigidbody2D> ();
+				rb.simulated = false;
+				tapeMode = "dragging";
+			}
+		}
+
+		//adjusts the tape's scale and rotation to the mouse position relative to its previous anchor point
+		if (tapeMode == "dragging") {			
+			mousePoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			float theta = Mathf.Rad2Deg * Mathf.Atan2 (anchorPoint.x - mousePoint.x, mousePoint.y - anchorPoint.y);
+			tapeInstance.rotation = Quaternion.Euler (Vector3.forward * theta);
+			tapeInstance.localScale = new Vector3 (tapeInstance.localScale.x, Vector3.Distance (mousePoint, anchorPoint) * scaleOverLengthRatio, 1);
+		
+			//sets tape mode to stuck and allows physics simulations again; trigger colliders will now be triggered
+			if (Input.GetMouseButtonUp (0)) {
+				rb.simulated = true;
+				tapeMode = "stuck";
+			}
+		}
+
+
+	}
+
+
+}
